d4ebfbe1021172eaa32dd56fb9c3d45e2a23acc2
Miroslav Talasek <miroslav.talasek@seznam.cz>
2018-09-17 20:35:48 +0200

    working intel hurray hurray

--- encoder_h264.sh
+++ encoder_h264.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#sudo apt install i965-va-driver-shaders/testing
 
 
 file="$1"
@@ -8,7 +7,7 @@ file="$1"
 echo "Encode movie $file "
 
 #ffmpeg  -stats   -i "$file"   -c:a libmp3lame -b:v 1800k -q:a 1 -c:v h264_nvenc  -profile:v main -level 4.0     "$TARGET_MOVIE"
-ffmpeg   -vaapi_device /dev/dri/renderD128 -i "$file"  -vf 'format=nv12,hwupload'  -c:v h264_vaapi  -b:v 3M -c:a libmp3lame  -q:a 1   "$TARGET_MOVIE"
+ffmpeg   -i "$file"  -c:v h264  -b:v 3M -c:a libmp3lame  -q:a 1   "$TARGET_MOVIE"
 #ffmpeg -threads 4 -hwaccel cuvid -i "$file" -c:v h264 -profile:v main -level:v 4.1 -preset slow -c:a libmp3lame  -q:a 1  "$TARGET_MOVIE"
 #-filter:v scale_npp=w=1920:h=1080:format=yuv420p:interp_algo=lanczos \
 
