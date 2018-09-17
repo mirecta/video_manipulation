035d28754439fad51324069fedec2b1785118157
Miroslav Talasek <miroslav.talasek@seznam.cz>
2018-09-17 20:39:23 +0200

    polish

--- encoder_h264_intel.sh
+++ encoder_h264_intel.sh
@@ -5,7 +5,7 @@
 
 file="$1"
     MOVIE_FILENAME="${file%.*}"
-    TARGET_MOVIE="${MOVIE_FILENAME}-h264.mkv"
+    TARGET_MOVIE="${MOVIE_FILENAME}-h264.avi"
 echo "Encode movie $file "
 
 #ffmpeg  -stats   -i "$file"   -c:a libmp3lame -b:v 1800k -q:a 1 -c:v h264_nvenc  -profile:v main -level 4.0     "$TARGET_MOVIE"
