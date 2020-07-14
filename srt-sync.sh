b38b6ae524302997bd63385bbc132c06cd5d9e79
Miroslav Talasek <miroslav.talasek@seznam.cz>
2020-07-14 22:01:31 +0200

    fix

--- srt-sync.sh
+++ srt-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Recoding....."
-find  . -name "*.srt" -print -exec  echo recode cp1250..utf8  {} \;
+find  . -name "*.srt" -print -exec recode cp1250..utf8  {} \;
 echo "Sync ....."
 scp -r ../tit  root@kodi:videos/
 echo "Deleting ......."
