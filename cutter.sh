124e5a91ecad95c7330d3635a0537adc9972086b
Miroslav Talasek <miroslav.talasek@gmail.com>
2022-08-05 15:25:11 +0200

    fix

--- cutter.sh
+++ cutter.sh
@@ -3,10 +3,10 @@
 extension="${1##*.}"
 filename="${1%.*}"
 
-if [ "$#" -ne 4 ]; then
-       	ffmpeg -ss $3 -i $1   -acodec copy -vcodec copy ${filename}_cut.${extension}
+if [ "$#" -ne 3 ]; then
+       	ffmpeg -ss $2 -i $1   -acodec copy -vcodec copy ${filename}_cut.${extension}
 else
 
-	ffmpeg -ss $3 -i $1 -to $4  -acodec copy -vcodec copy ${filename}_cut.${extension}
+	ffmpeg -ss $2 -i $1 -to $3  -acodec copy -vcodec copy ${filename}_cut.${extension}
 
 fi
